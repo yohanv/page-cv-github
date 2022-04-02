e61ec1cb6622c14205304f6cf958e25c07165e92
yohan VACHET <yohan-v@hotmail.fr>
2022-04-02 12:48:22 +0200

    autre test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# Je m'appel yohan vachet je suis entrain d'apprendre a utiliser git et github.
+# Je m'appel yohan v***** je suis entrain d'apprendre a utiliser git et github.
 
 
 def print_hi(name):
